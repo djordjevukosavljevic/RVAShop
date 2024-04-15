3ad428314184c6f312bbd7b610550fdf22662254
djordje.vukosavljevic01@gmail.com <djordje.vukosavljevic01@gmail.com>
2024-04-15 09:52:32 +0200

    Initial

--- RVAProdavnica.Web/Controllers/BaseController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace RVAProdavnica.Web.Controllers
-{
-    public class BaseController : Controller 
-    {
-        
-    }
-}
--- RVAProdavnica.Web/wwwroot/js/products.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-
-    let products = document.getElementById("")
-
-})
